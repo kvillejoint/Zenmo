f85359d3b8dbc1f985eb04651f087112bea22dc6
Jennifer Lara <jenniferlara@Jennifers-MacBook-Pro.local>
2017-07-26 09:55:35 -0400

    setup DB connection and created User model

---
+++ models/users.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+
+// Requires Sequelize library
+var Sequelize = require("sequelize");
+// Requires our connection to the DB
+var sequelize = require("../config/connection.js");
+
+// Creates a "User" model that matches up with DB
+var User = sequelize.define("user", {
+    username: {
+        type: Sequelize.STRING
+    },
+    password: {
+        type: Sequelize.STRING
+    },
+    first_name: {
+        type: Sequelize.STRING
+    },
+    last_name: {
+        type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    },
+    balance: {
+        type: Sequelize.DECIMAL
+    },
+    confirmation_code: {
+        type: Sequelize.INTEGER
+    }
+});
+
+// Syncs with DB
+User.sync();
+
+// Makes the User Model available for other files (will also create a table)
+module.exports = User;
