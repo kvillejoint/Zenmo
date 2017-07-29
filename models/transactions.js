9e2086919c40a084febdc0d4c7dc4da438e2fc12
Jennifer Lara <jenniferlara@Jennifers-MacBook-Pro.local>
2017-07-29 12:14:56 -0400

    added associate logic

--- models/transactions.js
+++ models/transactions.js
@@ -9,14 +9,7 @@ const sequelize = require("../config/connection.js");
 // Creates a "Transaction" model that matches up with DB
 module.exports = function(sequelize, DataTypes) {
         const Transaction = sequelize.define('Transaction', {
-                payer: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: {
-                        len: [1]
-                    }
-                },
-                payee: {
+                payer_email: {
                     type: DataTypes.STRING,
                     allowNull: false,
                     validate: {
@@ -37,25 +30,30 @@ module.exports = function(sequelize, DataTypes) {
                         len: [1]
                     }
                 },
-                {
+                payee_email: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    validate: {
+                        len: [1]
+                    }
+                },
+                    {
                     timestamps: false
                 });
-        
-        Transaction.associate = function(models) {
-            Transaction.belongsTo(models.User, {
-                as: 'payer',
-                foreignKey: {
-                   //this should be set to the payer as one foreign key and payee as second foreign key using aliases
-                    allowNull: false
-                },
-                as: 'payee',
-                foreignKey: {
-                    allowNull: false
-                }
-            });
+
+            Transaction.associate = function(models) {
+                Transaction.belongsTo(models.User, {
+                    as: 'payer',
+                    foreignKey: {
+                        //this should be set to the payer as one foreign key and payee as second foreign key using aliases
+                        allowNull: false
+                    },
+                    as: 'payee',
+                    foreignKey: {
+                        allowNull: false
+                    }
+                });
+            };
+
+            return Transaction;
         };
-
-        return Transaction;
-        }
-
-
--- models/users.js
+++ models/users.js
@@ -7,31 +7,35 @@ const Sequelize = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 // Creates a "User" model that matches up with DB
-const User = sequelize.define("user", {
-    first_name: {
-        type: Sequelize.STRING
-    },
-    last_name: {
-        type: Sequelize.STRING
-    },
-    phone: {
-        type: Sequelize.INTEGER
-    },
-    email: {
-        type: Sequelize.STRING
-    },
-    balance: {
-        type: Sequelize.DECIMAL
-    },
-    confirmation_code: {
-        type: Sequelize.INTEGER
-    },
-}, {
-    timestamps: false
-});
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define("User", {
+        first_name: {
+            type: Sequelize.STRING
+        },
+        last_name: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        balance: {
+            type: Sequelize.DECIMAL
+        },
+        confirmation_code: {
+            type: Sequelize.INTEGER
+        },
+    }, {
+        timestamps: false
+    });
 
-// Syncs with DB
-User.sync();
 
-// Makes the User Model available for other files (will also create a table)
-module.exports = User;
+    User.associate = function(models) {
+        User.hasMany(models.Transaction, {
+            onDelete: "cascade"
+        });
+
+    };
+
+    return User;
+
+};
